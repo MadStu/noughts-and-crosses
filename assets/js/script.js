ce30b22087fa9754dcd162c7958075fe761f7c52
Stuart Raynor <madstu@gmail.com>
2021-11-05 02:03:16 +0000

    Add function outlines to work out the logic

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+/**
+ * Add the X or O to the game board
+ */
+function setSquare() {
+
+}
+
+/**
+ * Check if there's 3 lines or if all squares are filled
+ */
+function checkWinner() {
+    
+}
+
+/**
+ * Change score if someone wins or draws
+ */
+function setScoreboard() {
+    
+}
+
+/**
+ * Show who's turn it is, short delay to simulate the computer thinking
+ */
+function changePlayer() {
+    
+}
+
+/**
+ * Find a free square to place Computer's X in
+ */
+function computerTurn() {
+    
+}
