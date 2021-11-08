b989f37b95b842206cd916730710ff531b9054b0
Stuart Raynor <madstu@gmail.com>
2021-11-08 16:11:02 +0000

    Update readme with bug

--- README.md
+++ README.md
@@ -107,6 +107,7 @@ There was a bug where if the screen became too narrow, the header image would re
 
 - ~~When the human player has won a game and there's still free spaces available, the computer takes a turn and records itself as having won before resetting the board.~~
 - ~~When the human player has won a game and there's still free spaces available, the computer takes another turn before resetting the board.~~
+- The computerSquareHard() function doesn't loop through the eligibleNumsArray.
 
 ## Deployment
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -217,33 +217,81 @@ function computerSquareHard(squareArray){
         }
 
         //Loop through the winning squares to check against player squares
-        let i = 0;
-        while (i < eligibleNumsArray.length) {
+        //let i = 0;
+        //while (i < eligibleNumsArray.length) {
+        for (let i = 0; i < eligibleNumsArray.length; i++) {
 
-            if (eligibleNumsArray[i].every((val) => playerArray.includes(val)) && ) {
+            console.log(i);
+
+            if (eligibleNumsArray.every((val) => playerArray.includes(val))) {
                 
-                //Match!
-                console.log(eligibleNumsArray[i]);
-                break;
+
+                console.log(eligibleNumsArray[i].toString());
+
+                let arrStr = eligibleNumsArray[i].toString();
+
+                if (arrStr === "1,2" && document.getElementById("square-3").innerHTML === ""){
+                    console.log("Putting X in square 3");
+                    return 3;
+                } else if (arrStr === "2,3" && document.getElementById("square-1").innerHTML === ""){
+                    console.log("Putting X in square 1")
+                } else if (arrStr === "1,3" && document.getElementById("square-2").innerHTML === ""){
+                    console.log("Putting X in square 2")
+                } else if (arrStr === "4,5" && document.getElementById("square-6").innerHTML === ""){
+                    console.log("Putting X in square 6")
+                } else if (arrStr === "5,6" && document.getElementById("square-4").innerHTML === ""){
+                    console.log("Putting X in square 4")
+                } else if (arrStr === "4,6" && document.getElementById("square-5").innerHTML === ""){
+                    console.log("Putting X in square 5")
+                } else if (arrStr === "7,8" && document.getElementById("square-9").innerHTML === ""){
+                    console.log("Putting X in square 9")
+                } else if (arrStr === "8,9" && document.getElementById("square-7").innerHTML === ""){
+                    console.log("Putting X in square 7")
+                } else if (arrStr === "7,9" && document.getElementById("square-8").innerHTML === ""){
+                    console.log("Putting X in square 8")
+                } else if (arrStr === "1,4" && document.getElementById("square-7").innerHTML === ""){
+                    console.log("Putting X in square 7")
+                } else if (arrStr === "4,7" && document.getElementById("square-1").innerHTML === ""){
+                    console.log("Putting X in square 1")
+                } else if (arrStr === "1,7" && document.getElementById("square-4").innerHTML === ""){
+                    console.log("Putting X in square 4")
+                } else if (arrStr === "2,5" && document.getElementById("square-8").innerHTML === ""){
+                    console.log("Putting X in square 8")
+                } else if (arrStr === "5,8" && document.getElementById("square-2").innerHTML === ""){
+                    console.log("Putting X in square 2")
+                } else if (arrStr === "2,8" && document.getElementById("square-5").innerHTML === ""){
+                    console.log("Putting X in square 5")
+                } else if (arrStr === "3,6" && document.getElementById("square-9").innerHTML === ""){
+                    console.log("Putting X in square 9")
+                } else if (arrStr === "6,9" && document.getElementById("square-3").innerHTML === ""){
+                    console.log("Putting X in square 3")
+                } else if (arrStr === "3,9" && document.getElementById("square-6").innerHTML === ""){
+                    console.log("Putting X in square 1")
+                } else if (arrStr === "1,5" && document.getElementById("square-9").innerHTML === ""){
+                    console.log("Putting X in square 9")
+                } else if (arrStr === "5,9" && document.getElementById("square-1").innerHTML === ""){
+                    console.log("Putting X in square 1")
+                } else if (arrStr === "1,9" && document.getElementById("square-5").innerHTML === ""){
+                    console.log("Putting X in square 1")
+                } else if (arrStr === "3,5" && document.getElementById("square-7").innerHTML === ""){
+                    console.log("Putting X in square 7")
+                } else if (arrStr === "5,7" && document.getElementById("square-3").innerHTML === ""){
+                    console.log("Putting X in square 3")
+                } else if (arrStr === "3,7" && document.getElementById("square-5").innerHTML === ""){
+                    console.log("Putting X in square 5")
+                } else {
+                    return computerSquareEasy(squareArray);
+                }
+
+                
+
+            } else {
+                return computerSquareEasy(squareArray);
             }
-            i++;
+            
+           // i++; 
         }
-
-
-        return computerSquareEasy(squareArray);
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
